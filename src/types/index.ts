528f25a91c84bb7599527d43acf467ee8ec19aec
Fuego <fuegozero35@gmail.com>
2024-12-03 17:59:09 +0800

    添加新的类型定义，支持 Widget、Page 和 GistTabs 结构

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,20 @@ export type ReactForwardRefComponent<F = any, T = any> = React.ForwardRefRenderF
  * 获取组件的props类型
  */
 export type GetReactComponentProps<T extends ReactComponent> = T extends ReactComponent<infer P> ? P : never;
+
+export type Widget<T = any> = {
+  data: T;
+  id: string;
+  name: string;
+};
+
+export type Page = {
+  id: string;
+  name: string;
+  widgets: Widget[];
+};
+
+export type GistTabs = {
+  pages: Page[];
+  updateAt: Date;
+};
