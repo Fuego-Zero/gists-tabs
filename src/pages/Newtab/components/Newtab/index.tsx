b6496a65f8029780de4195a260d347b82c4091de
Fuego <fuegozero35@gmail.com>
2024-12-05 15:53:06 +0800

    重构 Newtab 组件，新增 useActivePage、useGistsTabs 和 usePagesHandler 自定义 Hook，优化页面状态管理，移除旧的 useActivePageHook

--- src/pages/Newtab/components/Newtab/index.tsx
+++ src/pages/Newtab/components/Newtab/index.tsx
@@ -2,11 +2,13 @@ import React, { useMemo } from 'react';
 
 import { DatePicker, Layout } from 'antd';
 
-import data from '@/data/data.json';
+import { MainProvider } from '@/context/MainContext';
 import withTheme from '@/theme/withTheme';
 
 import HeaderMenu from './components/HeaderMenu';
-import useActivePageHook from './hooks/useActivePageHook';
+import useActivePage from './hooks/useActivePage';
+import useGistsTabs from './hooks/useGistsTabs';
+import usePagesHandler from './hooks/usePagesHandler';
 
 import type { Page } from '@/types';
 
@@ -15,21 +17,24 @@ import './index.scss';
 const { Header, Content } = Layout;
 
 const Newtab = () => {
-  const [activePageId, setActivePageId] = useActivePageHook();
-
-  const pages = useMemo<Page[]>(() => data.pages, []);
+  const [gistsTabs, setGistTabs] = useGistsTabs();
+  const [activePageId, setActivePageId] = useActivePage(gistsTabs);
+  const pagesHandler = usePagesHandler(gistsTabs, setGistTabs);
+  const pages = useMemo<Page[]>(() => gistsTabs.pages, [gistsTabs]);
 
   return (
-    <Layout>
-      <Header className="sticky top-0 z-10 w-full flex items-center">
-        <HeaderMenu activePageId={activePageId} pages={pages} setActivePageId={setActivePageId} />
-      </Header>
-      <Content>
-        <div className="App text-red-600 h-[100vh]">
-          <DatePicker />
-        </div>
-      </Content>
-    </Layout>
+    <MainProvider>
+      <Layout>
+        <Header className="sticky top-0 z-10 w-full flex items-center">
+          <HeaderMenu {...pagesHandler} activePageId={activePageId} pages={pages} setActivePageId={setActivePageId} />
+        </Header>
+        <Content>
+          <div className="App text-red-600 h-[100vh]">
+            <DatePicker />
+          </div>
+        </Content>
+      </Layout>
+    </MainProvider>
   );
 };
 
