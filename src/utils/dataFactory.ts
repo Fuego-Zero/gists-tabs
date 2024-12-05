3ef48a4e1c5385edb8a18daade3527a027edeb7a
Fuego <fuegozero35@gmail.com>
2024-12-05 16:02:39 +0800

    新增数据工厂工具，包含创建页面和部件的功能；添加深拷贝工具函数

---
+++ src/utils/dataFactory.ts
@@ -0,0 +1,34 @@
+import type { Page, Widget, WidgetType } from '@/types';
+
+export const createId = () => new Date().valueOf().toString(32);
+
+/**
+ * 创建页面
+ */
+export const createPage = (name: Page['name'], widgets: Page['widgets'] = []): Page => ({
+  id: createId(),
+  name,
+  widgets,
+});
+
+const WIDGET_DATA: Record<WidgetType, any> = {
+  bookmarks: [],
+  clocks: [],
+};
+
+/**
+ * 创建部件
+ */
+export const createTool = (
+  name: Widget['name'],
+  type: Widget['type'],
+  row: Widget['row'],
+  col: Widget['col'],
+): Widget => ({
+  id: createId(),
+  name,
+  type,
+  row,
+  col,
+  data: WIDGET_DATA[type],
+});
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export const clone = (data: any) => structuredClone(data);
