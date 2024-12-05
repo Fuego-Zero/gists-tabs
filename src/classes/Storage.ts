9d5adaab7b0415109aa43a11aab3b55a08b5ef80
Fuego <fuegozero35@gmail.com>
2024-12-05 15:52:29 +0800

    重构 Storage 类，使用枚举管理存储键，优化获取和设置 gists 相关数据的方法

--- src/classes/Storage.ts
+++ src/classes/Storage.ts
@@ -1,28 +1,45 @@
+import type { GistsTabs } from '@/types';
+
+enum storageKeys {
+  activePageId = 'activePageId',
+  gistsTabs = 'gistsTabs',
+  gistsToken = 'gistsToken',
+}
+
 class Storage {
   private get(keys: string | string[]) {
     return chrome.storage.local.get(keys);
   }
 
-  private set(items: Record<string, any>): Promise<void> {
-    return chrome.storage.local.set(items);
+  private set(key: keyof typeof storageKeys, value: any): Promise<void> {
+    return chrome.storage.local.set({ [key]: value });
   }
 
   async getActivePageId(): Promise<string> {
-    const res = await this.get('activePageId');
-    return res.activePageId;
+    const res = await this.get(storageKeys.activePageId);
+    return res[storageKeys.activePageId];
+  }
+
+  async getGistsTabs(): Promise<GistsTabs> {
+    const res = await this.get(storageKeys.gistsTabs);
+    return res[storageKeys.gistsTabs];
   }
 
   async getGistsToken(): Promise<string> {
-    const res = await this.get('gistsToken');
-    return res.gistsToken;
+    const res = await this.get(storageKeys.gistsToken);
+    return res[storageKeys.gistsToken];
   }
 
   async setActivePageId(pageId: string) {
-    return this.set({ activePageId: pageId });
+    return this.set(storageKeys.activePageId, pageId);
+  }
+
+  async setGistsTabs(data: GistsTabs) {
+    return this.set(storageKeys.gistsTabs, data);
   }
 
   async setGistsToken(token: string) {
-    return this.set({ gistsToken: token });
+    return this.set(storageKeys.gistsToken, token);
   }
 }
 
