d6c62ae36725418a8c4b03d53025d37b86fa0ed0
Fuego <FuegoZero35@gmail.com>
2025-01-06 17:23:37 +0800

    移除不必要的 React 类型定义，简化类型文件

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,5 @@
-import type React from 'react';
-
 import type { Bookmarks } from './widget/bookmark';
 import type { Clocks } from './widget/clock';
-
-export type ReactComponent<T = any> = React.ComponentType<T>;
-export type ReactForwardRefComponent<F = any, T = any> = React.ForwardRefRenderFunction<F, T>;
-
-/**
- * 获取组件的props类型
- */
-export type GetReactComponentProps<T extends ReactComponent> = T extends ReactComponent<infer P> ? P : never;
 
 type WidgetTypeMap = {
   bookmarks: Bookmarks;
