fb8b2d443fccbe5899b67bdff4f9276c3d06d242
Fuego <FuegoZero35@gmail.com>
2025-01-08 09:57:18 +0800

    更新 Newtab 组件，移除 DatePicker，新增 ContentArea 组件以展示小部件

--- src/pages/Newtab/components/Newtab/index.tsx
+++ src/pages/Newtab/components/Newtab/index.tsx
@@ -1,18 +1,18 @@
 import React, { useMemo } from 'react';
 
-import { DatePicker, Layout } from 'antd';
+import { Layout } from 'antd';
 
 import { MainProvider } from '@/context/MainContext';
 import withTheme from '@/theme/withTheme';
 
+import ContentArea from './components/ContentArea';
 import HeaderMenu from './components/HeaderMenu';
 import useActivePage from './hooks/useActivePage';
 import useGistsTabs from './hooks/useGistsTabs';
 import usePagesHandler from './hooks/usePagesHandler';
+import useWidgetsHandler from './hooks/useWidgetsHandler';
 
 import type { Page } from '@/types';
-
-import './index.scss';
 
 const { Header, Content } = Layout;
 
@@ -20,18 +20,18 @@ const Newtab = () => {
   const [gistsTabs, setGistTabs] = useGistsTabs();
   const [activePageId, setActivePageId] = useActivePage(gistsTabs);
   const pagesHandler = usePagesHandler(gistsTabs, setGistTabs);
+  const { widgets, ...widgetsHandler } = useWidgetsHandler(gistsTabs, setGistTabs, activePageId);
+
   const pages = useMemo<Page[]>(() => gistsTabs.pages, [gistsTabs]);
 
   return (
     <MainProvider>
-      <Layout>
+      <Layout className="min-h-[100vh]">
         <Header className="sticky top-0 z-10 w-full flex items-center">
           <HeaderMenu {...pagesHandler} activePageId={activePageId} pages={pages} setActivePageId={setActivePageId} />
         </Header>
-        <Content>
-          <div className="App text-red-600 h-[100vh]">
-            <DatePicker />
-          </div>
+        <Content className="p-[16px] min-h-full">
+          <ContentArea {...widgetsHandler} widgets={widgets} />
         </Content>
       </Layout>
     </MainProvider>
