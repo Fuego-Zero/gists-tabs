20fdb1f86327910d1c7f969da02f537e2afb915c
Fuego <FuegoZero35@gmail.com>
2025-01-09 16:34:27 +0800

    新增 Gists 存储管理功能，检查和同步云端 gists 数据

--- src/classes/Storage.ts
+++ src/classes/Storage.ts
@@ -1,3 +1,6 @@
+import { getAllGists, getGists, initGistsId, patchGists } from '@/api/gists';
+import { GISTS_FILE_DESCRIPTION } from '@/config';
+
 import type { GistsTabs } from '@/types';
 
 enum storageKeys {
@@ -7,6 +10,30 @@ enum storageKeys {
 }
 
 class Storage {
+  private gistsTabsId: string = '';
+  private init = this.checkGistTabsFile();
+
+  /**
+   * 检查云端是否存在 gists_tabs_data.json 文件，如果不存在就创建一个
+   */
+  private async checkGistTabsFile() {
+    const allGists = await getAllGists();
+    const { length } = allGists;
+
+    if (length === 0) {
+      this.gistsTabsId = await initGistsId();
+      return;
+    }
+
+    const gists = allGists.find((item) => item.description === GISTS_FILE_DESCRIPTION);
+    if (!gists) {
+      this.gistsTabsId = await initGistsId();
+      return;
+    }
+
+    this.gistsTabsId = gists.id;
+  }
+
   private get(keys: string | string[]) {
     return chrome.storage.local.get(keys);
   }
@@ -15,12 +42,39 @@ class Storage {
     return chrome.storage.local.set({ [key]: value });
   }
 
+  private async syncGists() {
+    try {
+      //* 0. 检查云端是否存在 gists_tabs_data.json 文件
+      await this.checkGistTabsFile();
+
+      //* 1. 获取云端的 gists 数据
+      const cloudData = await getGists(this.gistsTabsId);
+      const localData = await this.getGistsTabs();
+
+      //* 2. 与本地数据进行比对
+      let newData;
+      if (cloudData.updateAt > localData.updateAt) {
+        //* 2.1 如果云端数据更新，就更新本地数据
+        newData = cloudData;
+      } else {
+        //* 2.2 如果本地数据更新，就更新云端数据
+        newData = localData;
+      }
+
+      //* 3. 更新云端数据
+      await patchGists(this.gistsTabsId, newData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   async getActivePageId(): Promise<string> {
     const res = await this.get(storageKeys.activePageId);
     return res[storageKeys.activePageId];
   }
 
   async getGistsTabs(): Promise<GistsTabs> {
+    await this.init;
     const res = await this.get(storageKeys.gistsTabs);
     return res[storageKeys.gistsTabs];
   }
@@ -35,7 +89,8 @@ class Storage {
   }
 
   async setGistsTabs(data: GistsTabs) {
-    return this.set(storageKeys.gistsTabs, data);
+    await this.set(storageKeys.gistsTabs, data);
+    this.syncGists();
   }
 
   async setGistsToken(token: string) {
