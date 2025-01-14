58b2d16d88f5de0309dc2126afe25166e4ddfe0b
Fuego <FuegoZero35@gmail.com>
2025-01-14 17:53:42 +0800

    添加 useGistTabs 自定义 Hook，管理 Gists 标签页的状态和数据同步

---
+++ src/hooks/useGistsTabs.ts
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import Storage from '@/classes/Storage';
+import { clone } from '@/utils';
+import { createPage } from '@/utils/dataFactory';
+
+import type { GistsTabs } from '@/types';
+
+export default function useGistTabs(): [gistsTabs: GistsTabs, setGistsTabs: (data: GistsTabs) => void] {
+  const [gistsTabs, setGistsTabs] = useState<GistsTabs>({
+    pages: [],
+    updateAt: -1,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let data = await Storage.getGistsTabs();
+
+      //* 如果没有数据，就需要走初始化数据流程
+      if (!data) {
+        console.log('初始化数据流程');
+
+        data = {
+          pages: [createPage('首页')],
+          updateAt: Date.now(),
+        };
+
+        await Storage.setGistsTabs(data);
+      }
+
+      setGistsTabs(data);
+    })();
+  }, []);
+
+  const setGistsTabsHandler = useCallback((data: GistsTabs) => {
+    data.updateAt = Date.now();
+    setGistsTabs(clone(data));
+    Storage.setGistsTabs(data);
+  }, []);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    (async function sync() {
+      await Storage.syncGists();
+      const data = await Storage.getGistsTabs();
+      if (data) setGistsTabs(data);
+      timer = setTimeout(sync, 2000);
+    })();
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return [gistsTabs, setGistsTabsHandler];
+}
