5ffe1cd7b64dfc10536383da04cba85797e10dd7
Fuego <FuegoZero35@gmail.com>
2025-03-05 17:04:27 +0800

    更新 analyzeURL 方法，添加错误处理和通知功能，以便在解析网址失败时使用默认书签名称和图标

--- src/pages/Newtab/components/Newtab/components/ContentArea/components/Bookmark/utils.ts
+++ src/pages/Newtab/components/Newtab/components/ContentArea/components/Bookmark/utils.ts
@@ -1,3 +1,5 @@
+import { notification } from 'antd';
+
 function extractFavicon(htmlText: string): null | string {
   const pattern = /<link[^>]*rel=["'](?:shortcut\s+icon|icon)["'][^>]*href=["']([^"']+)["'][^>]*>/i;
   const matches = htmlText.match(pattern);
@@ -6,19 +8,29 @@ function extractFavicon(htmlText: string): null | string {
 }
 
 export const analyzeURL = async (url: string) => {
-  const htmlText = await fetch(url).then(async (res) => {
-    const { status } = res;
-    const text = res.text();
-    if (status >= 200 && status < 300) return text;
-    return Promise.reject(text);
-  });
+  let title = '默认书签名称';
+  let icon = null;
 
-  const [title = '书签名称'] = htmlText.match(/(?<=<title.*>).*(?=<\/title>)/g) || [];
-  let icon = extractFavicon(htmlText);
+  try {
+    const htmlText = await fetch(url).then(async (res) => {
+      const { status } = res;
+      const text = res.text();
+      if (status >= 200 && status < 300) return text;
+      return Promise.reject(text);
+    });
 
-  if (icon && !icon.startsWith('http')) {
-    const base = new URL(icon, url);
-    icon = base.href;
+    title = (htmlText.match(/(?<=<title.*>).*(?=<\/title>)/g) || [])[0] ?? title;
+    icon = extractFavicon(htmlText);
+
+    if (icon && !icon.startsWith('http')) {
+      const base = new URL(icon, url);
+      icon = base.href;
+    }
+  } catch (error) {
+    notification.error({
+      message: '解析失败',
+      description: '无法解析该网址，采用默认名称和图标',
+    });
   }
 
   return {
